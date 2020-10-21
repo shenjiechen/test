47a07b3211a3e8a7489731173a7971b77459a3e7
shenjiechen <shenjiechen@126.com>
2020-10-21 13:29:49 +0800

    兴建一个类

---
+++ web/src/cn/shenjiechen/app/Application.java
@@ -0,0 +1,32 @@
+package cn.shenjiechen.app;
+
+import java.util.ArrayList;
+
+public class Application {
+
+    public static void main(String[] args) {
+        ArrayList<String> strList = new ArrayList<>();
+        strList.add("fsdfds");
+        strList.add("sfds");
+        strList.add("fsd");
+        strList.add("sfdfsdg");
+        strList.add("gaetrsfd");
+        strList.forEach(item -> {
+            System.out.println(item);
+        });
+
+        System.out.print("Hello github.");
+    }
+    
+    public int add(int num1, int num2) {
+        return num1 + num2;
+    }
+    
+    public int sub(int num1, int num2) {
+        return num1 - num2;
+    }
+
+    public int sub2(int num1, int num2) {
+        return num1 - num2;
+    }
+}
---
+++ web/src/cn/shenjiechen/app/Demo.java
@@ -0,0 +1,12 @@
+package cn.shenjiechen.app;
+
+public class Demo {
+
+    public int add(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public int sub(int num1, int num2) {
+        return num1 - num2;
+    }
+}
