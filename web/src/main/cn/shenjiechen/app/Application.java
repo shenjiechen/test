2ec456824ddbdd2c90c74176dae504884e3874be
加七模二左移三 <35052262+shenjiechen@users.noreply.github.com>
2020-10-21 13:21:20 +0800

    更新文件
    添加sub方法啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊

--- web/src/main/cn/shenjiechen/app/Application.java
+++ web/src/main/cn/shenjiechen/app/Application.java
@@ -21,4 +21,8 @@ public class Application {
     public int add(int num1, int num2) {
         return num1 + num2;
     }
+    
+    public int sub(int num1, int num2) {
+        return num1 - num2;
+    }
 }
